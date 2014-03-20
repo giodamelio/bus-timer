0dc19c84c32eebecfba0241e56ce7ff74b4d6a8d
Gio d'Amelio <giodamelio@gmail.com>
2014-03-19 17:10:31 -0700

    7 Segment Hello World!

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print "Hello World!"
+from Adafruit_Seven_Segment_Backback import Adafruit_7Segment
+
+# Init our display
+display = Adafruit_7Segment.SevenSegment(address=0x70)
+
+display.writeDigit(0, 0);
+display.writeDigit(1, 0);
+display.writeDigit(3, 0);
+display.writeDigit(4, 0);
 
