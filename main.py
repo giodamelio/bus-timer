9d94532506c663058acfff664ceacf664527f16c
Gio d'Amelio <giodamelio@gmail.com>
2014-03-20 17:32:01 -0700

    version 1.0

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import argparse
 import time
+from threading import Timer
 
 import requests
 
 class BusTimer():
     def __init__(self, options):
         self.options = options
-        self.writeTime(self.getNextTime(2275, 8))
+        self.refresh()
+        
+    def refresh(self):
+        Timer(self.options.interval, self.refresh, ()).start()
+        
+        time = self.getNextTime(self.options.stopId, self.options.busId)
+        
+        print "Next {0} arives in {1} minutes".format(self.options.busId, time)
+
+        self.writeTime(time)
     
     def getNextTime(self, routeId, busId):
         data = requests.get("http://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-stop/1_%s.json" % routeId,
@@ -28,10 +38,21 @@ class BusTimer():
             from Adafruit_Seven_Segment_Backback import Adafruit_7Segment
             # Init our display
             display = Adafruit_7Segment.SevenSegment(address=0x70)
-            display.writeDigit(0, str(time).zfill(4)[0])
-            display.writeDigit(1, str(time).zfill(4)[1])
-            display.writeDigit(3, str(time).zfill(4)[2])
-            display.writeDigit(4, str(time).zfill(4)[3])
+
+            # Check if our nunber is negitive
+            negative = True if time < 0 else False
+
+            # Remove the negitive value
+            time = abs(time)
+
+            # Convert the time to a string and fill extra digits
+            time = str(time).zfill(4)
+
+            display.writeDigit(0, int(time[0]))
+            display.writeDigit(1, int(time[1]))
+            display.setColon(True)
+            display.writeDigit(3, int(time[2]))
+            display.writeDigit(4, int(time[3]), negative)
         else:
             # No hardware display
             print time
@@ -43,7 +64,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     
     # Get the stop id 
-    parser.add_argument("--stop-id", "-s", dest="id", help="Set the stop id")
+    parser.add_argument("--stop-id", "-s", dest="stopId", default=2275, help="Set the stop id")
+
+    # Get the bus id 
+    parser.add_argument("--bus-id", "-b", dest="busId", default=8, help="Set the bus id")
+
+    # Get the refresh interval
+    parser.add_argument("--refresh-interval", "-i", dest="interval", default=30, help="Set the refresh interval in seconds")
 
     # Do we have a hardware diaplay
     parser.add_argument("--no-display", "-nd", dest="noDisplay", action="store_true", help="Do we have a hardware display")
